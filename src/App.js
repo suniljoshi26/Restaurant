38c84eae302b63a2c794e0bc11dc9b6f97c54caa
Sunil Joshi <joshisunil2601@gmail.com>
2023-01-09 19:07:43 +0530

    header component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="flex flex-col w-screen h-auto">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
+// npm install firebase
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="fixed z-20 w-screen p-6 px-16 bg-slate-300">
+      {/* desktop & tablat */}
+      <div className=""></div>
+      {/* mobile */}
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Header;
